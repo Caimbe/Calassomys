9416b82db58b150578475a7ab09de9f3d915e48e
André Rocha <d3roch4@gmail.com>
2014-08-22 16:35:01 -0300

    Ta funcionando :)

---
+++ webapps/helloCalassomys/src/webappconfig.cpp
@@ -0,0 +1,5 @@
+#include "webappconfig.h"
+
+WebAppConfig::WebAppConfig()
+{
+}
---
+++ webapps/helloCalassomys/src/webappconfig.h
@@ -0,0 +1,10 @@
+#ifndef WEBAPPCONFIG_H
+#define WEBAPPCONFIG_H
+
+class WebAppConfig
+{
+public:
+    WebAppConfig();
+};
+
+#endif // WEBAPPCONFIG_H
